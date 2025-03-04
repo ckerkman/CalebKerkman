2f618fb6a50db32eecfb95792e840a8018a4fee7
ckerkman <kerkmancaleb@gmail.com>
2025-03-04 16:31:47 -0600

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-addEventListener('fetch', event => {
-  event.respondWith(
-    fetch('https://your-website-url.com/assets/home.html')  // Path to your home.html file
-  );
-});
-exprorts.build = series(scssTask, jsTask);
